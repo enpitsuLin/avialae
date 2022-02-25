afa93f5841f9e6a70a496f47d1ed0457b0957963
enpitsulin <lldslm@vip.qq.com>
2022-02-25 14:51:00 +0800

    feat: entry and deps

---
+++ mod.ts
@@ -0,0 +1,4 @@
+import { commonServer, hmrServer } from "./src/server/index.ts";
+const { args } = Deno;
+
+commonServer();
---
+++ src/deps.ts
@@ -0,0 +1,5 @@
+import * as path from "https://deno.land/std@0.126.0/path/mod.ts";
+
+import * as mod from "https://deno.land/std@0.126.0/fmt/colors.ts";
+
+export { path, mod };
